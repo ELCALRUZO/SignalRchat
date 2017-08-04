8848b10257e72123ff9169c0059c4ec5dff6eb6a
T1128 <T1128@BPR-018>
2017-08-04 09:23:06 +0100

    Add project files.

---
+++ SignalRchat/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalRchat.Startup))]
+namespace SignalRchat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
